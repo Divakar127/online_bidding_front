382e8aec93bc30971ad7d1503ec2e15d1b2bef8d
Divakar TG <144575008+Divakar127@users.noreply.github.com>
2024-10-23 02:40:17 +0530

    Update CreateAuctionItem.jsx

--- src/components/CreateAuctionItem.jsx
+++ src/components/CreateAuctionItem.jsx
@@ -19,7 +19,7 @@ const CreateAuctionItem = () => {
 		if (token) {
 			try {
 				await axios.post(
-					`${import.meta.env.VITE_API_PATH}/api/auctions`, // Updated to use environment variable
+					"/api/auctions",
 					{ title, description, startingBid, endDate },
 					{
 						headers: { Authorization: `Bearer ${token}` },
